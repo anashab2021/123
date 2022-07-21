3cb1c426ff513370ea71d6b1ae0ca71857cb4eaf
anashab2021 <a-sad87@hotmail.com>
2022-07-21 16:30:54 +0300

    new task

---
+++ app/public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "mysql8-container";
+$database = "mysql";
+$username = "root";
+$password = "secret";
+$charset = "utf8mb4";
+
+try {
+
+$dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
+$pdo = new PDO($dsn, $username, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo "Connection ok"; 
+
+return $pdo;
+
+}
+
+catch (PDOException $e)
+
+{
+echo "Connection failed: ". $e->getMessage();
+}
+
+?>
